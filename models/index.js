b30274bffe8c96bc97c836e2249a0dde95940ba5
youngff43 <99932948+youngff43@users.noreply.github.com>
2022-06-05 00:20:47 -0400

    made product routes

--- models/index.js
+++ models/index.js
@@ -4,11 +4,17 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongs To Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
-// Products belongToMany Tags (through ProductTag)
+// Products belongTo Many Tags (through ProductTag)
 
 // Tags belongToMany Products (through ProductTag)
 
